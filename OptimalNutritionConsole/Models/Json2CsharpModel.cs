399fc411a26e87b55da2e8b76853cad5f906adc2
j.curtis.webb <j.curtis.webb@gmail.com>
2018-11-15 06:42:03 -0500

    prototype console application: -Gets json from USDA uses hardcoded URL -Deserializes json into food array

---
+++ OptimalNutritionConsole/Models/Json2CsharpModel.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimalNutritionConsole.Models
+{
+    public class Desc
+    {
+        public string ndbno { get; set; }
+        public string name { get; set; }
+        public string ds { get; set; }
+        public string manu { get; set; }
+        public string ru { get; set; }
+    }
+
+    public class Nutrient
+    {
+        public string nutrient_id { get; set; }
+        public string name { get; set; }
+        public string derivation { get; set; }
+        public string group { get; set; }
+        public string unit { get; set; }
+        public string value { get; set; }
+        public List<object> measures { get; set; }
+    }
+
+    public class Food2
+    {
+        public string sr { get; set; }
+        public string type { get; set; }
+        public Desc desc { get; set; }
+        public List<Nutrient> nutrients { get; set; }
+        public List<object> footnotes { get; set; }
+    }
+
+    public class Food
+    {
+        public Food2 food { get; set; }
+        public string error { get; set; }
+    }
+
+    public class RootObject
+    {
+        [JsonProperty("foods")]
+        public List<Food> foods { get; set; }
+        public int count { get; set; }
+        public int notfound { get; set; }
+        public double api { get; set; }
+    }
+}
---
+++ OptimalNutritionUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OptimalNutritionUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("OptimalNutritionUnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6a5c3b5-6a94-4a96-924c-1d2557ba81f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
